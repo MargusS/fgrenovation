82911f7b0005acdc8086256bbb2fd7a3e9387f15
airam_lemes <airam.lemes101@alu.ulpgc.es>
2026-06-21 20:08:41 +0100

    Performance: Optimize images

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: false,
+    },
+};
 
-export default nextConfig
+export default nextConfig;
